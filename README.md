6e19c0f70b53185fd3e653625565277a8656e163
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2025-11-21 01:08:04 +1100

    pc highlighter test 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# CobbleAid
+
+Client-side helpers for Cobblemon.
+
+## PC highlight filter
+
+- Configure `pcHighlighting` in the in-game config screen (Mod Menu + AutoConfig).
+- Enable any of the toggles to highlight matching Pokémon in the PC.
+- Tweak the associated color pickers to customize the background tint (ARGB).
+
--- src/client/java/cc/turtl/cobbleaid/config/ModConfig.java
+++ src/client/java/cc/turtl/cobbleaid/config/ModConfig.java
@@ -11,10 +11,33 @@ public class ModConfig implements ConfigData {
 
     @ConfigEntry.Gui.Tooltip public boolean debugMode = false;
 
+    @ConfigEntry.Gui.CollapsibleObject
+    public PcHighlighting pcHighlighting = new PcHighlighting();
+
     // custom validation to run on save and load
     public ModConfig validate_fields() {
         // put logic here
 
         return this;
+    }
+
+    public static class PcHighlighting {
+        @ConfigEntry.Gui.Tooltip
+        public boolean highlightHiddenAbility = true;
+
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int hiddenAbilityColor = 0x804E6CE7;
+
+        @ConfigEntry.Gui.Tooltip
+        public boolean highlightMaxIV = true;
+
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int maxIvColor = 0x80F6C667;
+
+        @ConfigEntry.Gui.Tooltip
+        public boolean highlightShiny = true;
+
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int shinyColor = 0x80FFB347;
     }
 }
