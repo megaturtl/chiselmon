22765ef4eaed01afebb6eb1fa501e37afbf59c13
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2025-12-27 02:25:04 +1100

    lang updates

--- src/main/java/cc/turtl/cobbleaid/api/predicate/PokemonEntityPredicates.java
+++ src/main/java/cc/turtl/cobbleaid/api/predicate/PokemonEntityPredicates.java
@@ -10,6 +10,7 @@ public final class PokemonEntityPredicates {
 
     public static final Predicate<PokemonEntity> IS_OWNED = entity -> entity.getOwnerUUID() != null;
 
+    // this is the best i can do rn but needs to be improved
     public static final Predicate<PokemonEntity> IS_PLUSHIE = entity -> {
         return (entity.getPokemon().getLevel() == 1 && entity.getPokemon().getExperience() == 0);
     };
