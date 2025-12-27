391c7a0bd41eafceeb69575dbab61c56d6c4c72a
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2025-12-28 02:52:08 +1100

    updated to 1.0.3-alpha

--- src/main/java/cc/turtl/cobbleaid/CobbleAid.java
+++ src/main/java/cc/turtl/cobbleaid/CobbleAid.java
@@ -20,7 +20,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class CobbleAid implements ClientModInitializer {
     public static final String MODID = "cobbleaid";
-    public static final String VERSION = "1.0.2-alpha";
+    public static final String VERSION = "1.0.3-alpha";
 
     private static CobbleAid INSTANCE;
     private volatile ICobbleAidServices services;
