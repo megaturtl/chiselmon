b6f22d05241f01d72511ac7c219bd3a730bee2c2
megaturtl <maniatyl@gmail.com>
2026-04-15 15:19:27 +1000

    Remove `WallpaperManager` class and migrate all  wallpaper management functionality into `MixinWallpaperEntry`

--- common/src/main/java/cc/turtl/chiselmon/mixin/MixinWallpaperEntry.java
+++ common/src/main/java/cc/turtl/chiselmon/mixin/MixinWallpaperEntry.java
@@ -1,21 +1,35 @@
 package cc.turtl.chiselmon.mixin;
 
 import cc.turtl.chiselmon.client.config.ChiselmonConfig;
-import cc.turtl.chiselmon.feature.pc.wallpaper.WallpaperManager;
+import cc.turtl.turtlshell.api.core.format.ColorLib;
+import com.cobblemon.mod.common.CobblemonSounds;
 import com.cobblemon.mod.common.client.gui.pc.PCGUI;
 import com.cobblemon.mod.common.client.gui.pc.WallpapersScrollingWidget;
+import com.cobblemon.mod.common.net.messages.server.storage.pc.RequestChangePCBoxWallpaperPacket;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static cc.turtl.chiselmon.core.util.format.ComponentUtilsKt.createComponent;
+
 @Mixin(value = WallpapersScrollingWidget.WallpaperEntry.class)
 public abstract class MixinWallpaperEntry {
+
+    @Unique
+    private static final Component HINT_COMPONENT = createComponent("APPLY ALL", ColorLib.INSTANCE.getYELLOW().getRGB(), true);
+    @Unique
+    private static final float HINT_SCALE = 0.70f;
 
     @Shadow(aliases = "this$0", remap = false)
     @Final
@@ -31,22 +45,41 @@ public abstract class MixinWallpaperEntry {
     @Inject(method = "render", at = @At("RETURN"))
     private void chiselmon$renderBulkHint(GuiGraphics guiGraphics, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovering, float partialTick, CallbackInfo ci) {
         if (ChiselmonConfig.INSTANCE.getGeneral().getModDisabled()) return;
+        if (!Screen.hasControlDown()) return;
 
-        WallpaperManager.renderBulkHint(guiGraphics, left, top, width, height);
+        guiGraphics.pose().pushPose();
+
+        float x = (left + width) - 49;
+        float y = (top + height) - 12;
+
+        guiGraphics.pose().translate(x, y, 100f);
+        guiGraphics.pose().scale(HINT_SCALE, HINT_SCALE, 1f);
+
+        guiGraphics.drawString(Minecraft.getInstance().font, HINT_COMPONENT, 0, 0, 0xFFFFFF);
+
+        guiGraphics.pose().popPose();
     }
 
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
     private void chiselmon$handleBulkClick(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
         if (ChiselmonConfig.INSTANCE.getGeneral().getModDisabled()) return;
+        if (!Screen.hasControlDown()) return;
+
+        boolean isAlt = Screen.hasShiftDown() && altWallpaper != null;
+        ResourceLocation appliedWallpaper = isAlt ? altWallpaper : wallpaper;
 
         PCGUI pcGui = this.outer.getPcGui();
-        if (WallpaperManager.handleBulkClick(pcGui, this.wallpaper, this.altWallpaper)) {
+        int boxCount = pcGui.getPc().getBoxes().size();
 
-            // Remove from unseen if we handle the bulk click for the first time the wallpaper is applied
-            pcGui.getUnseenWallpapers().remove(this.wallpaper);
-            this.isNew = false;
-
-            cir.setReturnValue(true);
+        for (int i = 0; i < boxCount; i++) {
+            pcGui.getPc().getBoxes().get(i).setWallpaper(appliedWallpaper);
+            new RequestChangePCBoxWallpaperPacket(pcGui.getPc().getUuid(), i, wallpaper, isAlt ? altWallpaper : null).sendToServer();
         }
+
+        pcGui.getUnseenWallpapers().remove(this.wallpaper);
+        this.isNew = false;
+
+        Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(CobblemonSounds.PC_CLICK, 1.0F));
+        cir.setReturnValue(true);
     }
 }
