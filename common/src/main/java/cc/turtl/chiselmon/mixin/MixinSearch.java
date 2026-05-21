f02be0eda770df51a8b8c7b38edc29703d96a266
megaturtl <64576737+megaturtl@users.noreply.github.com>
2026-05-21 16:21:08 +1000

    Now redirects egg dummy in PC filter searches

---
+++ common/src/main/java/cc/turtl/chiselmon/mixin/MixinSearch.java
@@ -0,0 +1,20 @@
+package cc.turtl.chiselmon.mixin;
+
+import cc.turtl.chiselmon.client.feature.eggspy.EggPreview;
+import com.cobblemon.mod.common.api.storage.pc.search.Search;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(Search.class)
+public class MixinSearch {
+
+    @ModifyVariable(method = "passes", at = @At("HEAD"), argsOnly = true, name = "pokemon")
+    @Nullable
+    private Pokemon replacePokemon(@Nullable Pokemon pokemon) {
+        if (pokemon == null) return null;
+        return EggPreview.forDisplay(pokemon);
+    }
+}
